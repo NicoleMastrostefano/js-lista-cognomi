c3a9da316bb00faa5b161b4bb258d18c6843e25c
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-11-16 16:11:17 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Lista Cognomi
+
+
+// Chiedere all’utente il cognome,
+var cognome = (prompt("Inserisci il tuo cognome (usa il maiuscolo per la prima lettera!)"));
+console.log(cognome);
+
+// inserirlo in un array con altri cognomi (Es: 'Pesce', ‘Castenetto’, 'Passaro', 'Grimaldi', ‘Rossi’, ‘Verdi’, ‘Bianchi’).
+
+var listaCognomi = ['Pesce', "Castenetto", 'Passaro', 'Grimaldi', "Mioli"];
+listaCognomi.push (cognome);
+
+// Stampa la lista ordinata alfabeticamente.
+listaCognomi.sort();
+for (var i = 0; i < listaCognomi.length; i++) {
+  console.log( i + ": " + listaCognomi[i]);
+}
+
+// Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova.
+//
+var posCognome = (listaCognomi.indexOf(cognome)) + 1;
+console.log("Posizione" + " " + listaCognomi.indexOf(cognome));
